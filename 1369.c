d5638c9663cbb16b68bb46d52f97a0a5141ce0db
Kim gyu rim <nuripiggy@gmail.com>
2023-09-14 22:11:04 +0900

    commit 1

---
+++ 1369.c
@@ -0,0 +1,14 @@
+#includr<stdio.h>
+
+int main()
+{
+    int x,y;
+    scanf("%d %d",&x, &y);
+    for(inr i=1; i<=x; i++, puts(--))
+        for(int j=1; j<=x; j++)
+            if(i==1 ||j==1||i==x||j==x (i+j-1)%y==0)
+            printf("*");
+        else printf(" " );
+    return 0;
+
+}
